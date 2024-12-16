d7b71bd1e4cc6e553cc1acf05467cf6eefc4226a
hartage82 <mohammad.sh8186@gmail.com>
2024-12-16 13:10:18 -0800

    Remove ignored files and update .gitignore

--- staticfiles/blog/js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-    document.querySelectorAll('.delete-post').forEach(button => {
-        button.addEventListener('click', function(e) {
-            if (!confirm('Are you sure you want to delete this post?')) {
-                e.preventDefault();
-            }
-        });
-    });
-
